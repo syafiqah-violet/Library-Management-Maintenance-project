f314ec5168fd2b7962768385a84bf6fec26e98cf
marsyaaa <marsyaamelia00@gmail.com>
2022-05-25 12:50:57 +0800

    Error message for new password == current password

--- admin/change-password.php
+++ admin/change-password.php
@@ -2,9 +2,12 @@
 session_start();
 include('includes/config.php');
 error_reporting(0);
+
 if (strlen($_SESSION['alogin']) == 0) {
   header('location:index.php');
-} else {
+} 
+
+else {
   if (isset($_POST['change'])) {
     $password = md5($_POST['password']);
     $newpassword = md5($_POST['newpassword']);
@@ -15,19 +18,27 @@ if (strlen($_SESSION['alogin']) == 0) {
     $query->bindParam(':password', $password, PDO::PARAM_STR);
     $query->execute();
     $results = $query->fetchAll(PDO::FETCH_OBJ);
-    if ($query->rowCount() > 0) {
+
+    if($newpassword == $password) {
+      $error=" New password cannot be the same as current password.";
+    }
+
+    else if ($query->rowCount() > 0) {
       $con = "update admin set Password=:newpassword where UserName=:username";
       $chngpwd1 = $dbh->prepare($con);
       $chngpwd1->bindParam(':username', $username, PDO::PARAM_STR);
       $chngpwd1->bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
       $chngpwd1->execute();
       $msg = "Your Password succesfully changed";
-    } else {
+    } 
+    
+    else {
       $error = "Your current password is wrong";
     }
   }
 
 ?>
+
   <!DOCTYPE html>
   <html xmlns="http://www.w3.org/1999/xhtml">
 
--- change-password.php
+++ change-password.php
@@ -2,37 +2,42 @@
 session_start();
 include('includes/config.php');
 error_reporting(0);
-if(strlen($_SESSION['login'])==0)
-    {   
-header('location:index.php');
-}
-else{ 
-if(isset($_POST['change']))
-  {
-$password=md5($_POST['password']);
-$newpassword=md5($_POST['newpassword']);
-$email=$_SESSION['login'];
-  $sql ="SELECT Password FROM tblstudents WHERE EmailId=:email and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
-if($query -> rowCount() > 0)
-{
-$con="update tblstudents set Password=:newpassword where EmailId=:email";
-$chngpwd1 = $dbh->prepare($con);
-$chngpwd1-> bindParam(':email', $email, PDO::PARAM_STR);
-$chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
-$chngpwd1->execute();
-$msg="Your Password succesfully changed";
-}
-else {
-$error="Your current password is wrong";  
-}
+
+if(strlen($_SESSION['login'])==0) {   
+  header('location:index.php');
 }
 
+else { 
+  if(isset($_POST['change'])) {
+    $password=md5($_POST['password']);
+    $newpassword=md5($_POST['newpassword']);
+    $email=$_SESSION['login'];
+    $sql ="SELECT Password FROM tblstudents WHERE EmailId=:email and Password=:password";
+    $query= $dbh -> prepare($sql);
+    $query-> bindParam(':email', $email, PDO::PARAM_STR);
+    $query-> bindParam(':password', $password, PDO::PARAM_STR);
+    $query-> execute();
+    $results = $query -> fetchAll(PDO::FETCH_OBJ);
+
+    if($newpassword == $password) {
+      $error=" New password cannot be the same as current password.";
+    }
+
+    else if($query -> rowCount() > 0) {
+      $con="update tblstudents set Password=:newpassword where EmailId=:email";
+      $chngpwd1 = $dbh->prepare($con);
+      $chngpwd1-> bindParam(':email', $email, PDO::PARAM_STR);
+      $chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
+      $chngpwd1->execute();
+      $msg="Your Password succesfully changed";
+    }
+
+    else {
+    $error="Your current password is wrong";  
+    }
+  }
 ?>
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
